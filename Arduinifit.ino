e88b30decfda8e646c32be614bcc6ae0efcd605b
aamott <54421854+aamott@users.noreply.github.com>
2024-03-30 14:05:26 -0600

    move config to separate file

--- Arduinifit.ino
+++ Arduinifit.ino
@@ -33,31 +33,7 @@
  * the potentiometer.
  ****************************************************************/
 
-/************************************************
-* User Config
-*/
-// Outputs
-#define speedPin 11
-#define inclineUpPin 9
-#define inclineDownPin 10
-
-// Inputs
-#define speedPotPin A2
-#define inclineUpBtn 4  // Buttons should be high by default, low when pushed
-#define inclineDownBtn 3
-#define inclineBtnInvert  // Uncomment if the button is low when pressed. Comment if button is high when pressed.
-// #define inclineSensePin 8 // switches on/off every step of incline. Not implemented yet.
-#define safetyKeyPin -1  // Safety key is a pin that outputs 3-5v when the key is inserted/attached. -1 to disable.
-
-// Max duty cycle defines max treadmill speed
-#define maxDutyCycle 0.55
-
-// PWM timing
-#define hertz 20
-
-/************************************************
-* End User Config
-*************************************************/
+#include "config.h"
 
 #ifdef inclineBtnInvert
 #define invert !
---
+++ config.h
@@ -0,0 +1,25 @@
+/************************************************
+* User Config
+*/
+// Outputs
+#define speedPin 11
+#define inclineUpPin 9
+#define inclineDownPin 10
+
+// Inputs
+#define speedPotPin A2
+#define inclineUpBtn 4  // Buttons should be high by default, low when pushed
+#define inclineDownBtn 3
+#define inclineBtnInvert  // Uncomment if the button is low when pressed. Comment if button is high when pressed.
+// #define inclineSensePin 8 // switches on/off every step of incline. Not implemented yet.
+#define safetyKeyPin -1  // Safety key is a pin that outputs 3-5v when the key is inserted/attached. -1 to disable.
+
+// Max duty cycle defines max treadmill speed
+#define maxDutyCycle 0.55
+
+// PWM timing
+#define hertz 20
+
+/************************************************
+* End User Config
+*************************************************/
